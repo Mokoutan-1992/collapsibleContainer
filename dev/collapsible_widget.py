5146f1cceff77a697b6812fbc568ccfa3770b00c
Rayshin <ohrsmt@gmail.com>
2022-07-09 20:05:49 +0900

    wip

--- dev/collapsible_widget.py
+++ dev/collapsible_widget.py
@@ -5,16 +5,9 @@ DPI_SCALE = get_logicaldpi() / 96.0
 
 
 class NewCollapsible(QtWidgets.QWidget):
-    SPEED = 300
+    SPEED = 100
 
     def __init__(self, title="", parent=None):
-        """ QCollapse is a collapsible widget with transition
-
-        Args:
-            parent (QWidget): parent widget for the QCollapseWidget
-            title (str): Title name for the widget
-        """
-
         super(NewCollapsible, self).__init__(parent)
 
         # create main layout
@@ -49,12 +42,6 @@ class NewCollapsible(QtWidgets.QWidget):
         self.arrow_button.clicked.connect(self.__run_animation)
 
     def setContentLayout(self, layout):
-        """ Applies the given layout to the scroll area widget
-
-        Args:
-            layout (QLayout): layout widget to add into the QCollapse
-        """
-
         # sets the layout into the scroll area
         self.content_area.setLayout(layout)
         self.content_height = layout.sizeHint().height()
@@ -63,7 +50,6 @@ class NewCollapsible(QtWidgets.QWidget):
         """ Runs the animation group
         """
         checked = self.arrow_button.isChecked()
-
         self.size_anim = QtCore.QPropertyAnimation(self.content_area, b"geometry")
         geometry = self.content_area.geometry()
         width = geometry.width()
@@ -72,27 +58,22 @@ class NewCollapsible(QtWidgets.QWidget):
         size_start = QtCore.QRect(x, y, width, int(not checked) * self.content_height)
         size_end = QtCore.QRect(x, y, width, checked * self.content_height)
 
-        print(self.windowTitle(), size_start)
-        print(self.windowTitle(), size_end)
-
         self.size_anim.setStartValue(size_start)
-        self.size_anim.setStartValue(size_end)
+        self.size_anim.setEndValue(size_end)
         self.size_anim.setDuration(self.SPEED)
 
         size_anim_curve = QtCore.QEasingCurve()
         if checked:
+            self.arrow_button.setArrowType(QtCore.Qt.DownArrow)
             size_anim_curve.setType(QtCore.QEasingCurve.InQuad)
         else:
+            self.arrow_button.setArrowType(QtCore.Qt.RightArrow)
+
             size_anim_curve.setType(QtCore.QEasingCurve.OutQuad)
         self.size_anim.setEasingCurve(size_anim_curve)
-        if checked:
-            self.arrow_button.setArrowType(QtCore.Qt.DownArrow)
-            self.content_area.setMaximumHeight(self.content_height)
-        else:
-            self.arrow_button.setArrowType(QtCore.Qt.RightArrow)
-            self.content_area.setMaximumHeight(0)
-        # self.size_anim.valueChanged.connect(self._forceResize)
-        # self.size_anim.start(QtCore.QAbstractAnimation.DeleteWhenStopped)
+
+        self.size_anim.valueChanged.connect(self._forceResize)
+        self.size_anim.start(QtCore.QAbstractAnimation.DeleteWhenStopped)
 
     def _forceResize(self, new_height):
-        self.setFixedHeight(new_height.height())
+        self.content_area.setFixedHeight(new_height.height())
